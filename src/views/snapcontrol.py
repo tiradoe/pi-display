af7ca483c23d7fadc760461e65ed2c0af69c6bbb
Edward Tirado Jr <ed@etdevelopment.net>
2022-01-17 21:14:47 -0700

    reorganized snapcontrol code and cleaned up group layout

--- src/views/snapcontrol.py
+++ src/views/snapcontrol.py
@@ -19,103 +19,89 @@ class SnapControlView(Gtk.FlowBox):
                 )
 
     def generate_view(self):
-        display = Gtk.FlowBox()
+        display= Gtk.ScrolledWindow()
+        groups_flowbox = Gtk.FlowBox()
 
-        display.set_valign(Gtk.Align.START)
-        display.set_selection_mode(Gtk.SelectionMode.NONE)
-        display.set_name("snapcontrol-grid")
+        groups_flowbox.set_valign(Gtk.Align.START)
+        groups_flowbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        groups_flowbox.set_name("snapcontrol-grid")
 
-        self.get_group_list(display)
+        self.update_group_list(groups_flowbox)
+        display.add(groups_flowbox)
 
-        scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.add(display)
-
-        return scrolled_window
-
-
-    def get_group_list(self, flowbox):
-
-        for group in self.server.groups:
-            group_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-            group_box.set_name("group-box")
-
-            # HEADER
-            group_header_box = Gtk.Box()
-            group_header_box.set_name("group-header")
-
-            group_switch = Gtk.Switch()
-            group_switch.set_name("switch")
-            group_switch.connect("notify::active", self.on_mute_toggle, {"type": "group", "group": group})
-            group_switch.set_state(not group.muted)
-
-            group_name = Gtk.Label(group.friendly_name)
-
-            group_header_box.pack_start(group_name, False, False, 20)
-            group_header_box.pack_end(group_switch, False, False, 1)
-
-            group_box.pack_start(group_header_box, False, False, 1)
-
-            streams = []
-            # DROPDOWN
-            stream_store = Gtk.ListStore(str, str)
-            for stream in self.server.streams:
-                streams.append(stream.friendly_name)
-                stream_store.append([group.identifier, stream.friendly_name])
-
-            source_dropdown = Gtk.ComboBox.new_with_model_and_entry(stream_store)
-            source_dropdown.set_entry_text_column(1)
-
-            # If the stream isn't found, just use the first one
-            # This can because by changing the stream name on the server
-            try:
-                source_dropdown.set_active(streams.index(group.stream))
-            except:
-                source_dropdown.set_active(0)
-
-            source_dropdown.connect("changed", self.on_stream_changed)
-
-            source_dropdown_context = source_dropdown.get_style_context()
-            source_dropdown_context.add_class("dropdown")
-
-            source_container = Gtk.Box()
-            source_container.pack_start(Gtk.Label("Source"), False, False, 1)
-            source_container.pack_end(source_dropdown, False, False, 1)
-            source_container.set_name("source-container")
-
-            group_box.pack_start(source_container, False, False, 1)
-            separator = Gtk.Separator()
-            group_box.pack_start(separator, False, False, 1)
-
-            separator_context = separator.get_style_context()
-            separator_context.add_class("separator")
-
-            #CLIENTS
-
-            group_box.pack_start(Gtk.Label("Clients"), False, False, 1)
-
-            for client_name in group.clients:
-                client_box = Gtk.Box()
-                client_box.set_name("client-box")
-
-                client_switch = Gtk.Switch()
-                client_switch.connect("notify::active", self.on_mute_toggle,  {"type": "client", "client": client_name})
-                client_switch.set_state(not self.server.client(client_name).muted)
-
-                name = Gtk.Label(self.server.client(client_name).friendly_name)
+        return display
 
 
-                client_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-                client_context = client_container.get_style_context()
-                client_context.add_class("client-container")
+    def get_clients_box(self, group):
+        client_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        client_context = client_container.get_style_context()
+        client_context.add_class("client-container")
 
-                client_box.pack_start(name, False, False, 10)
-                client_box.pack_end(client_switch, False, False, 1)
+        clients_label = Gtk.Label("Clients")
+        clients_label_context = clients_label.get_style_context()
+        clients_label_context.add_class("title")
+        clients_label.set_halign(Gtk.Align.START)
+        client_container.pack_start(clients_label, False, False, 0)
 
-                client_container.pack_start(client_box, False, False, 0)
+        for client_name in group.clients:
+            client_box = Gtk.Box()
+            client_box_context = client_box.get_style_context()
+            client_box_context.add_class("client-box")
 
-                group_box.pack_start(client_container, False, False, 1)
+            client_switch = Gtk.Switch()
+            client_switch.connect(
+                "notify::active",
+                self.on_mute_toggle,
+                {"type": "client", "client": client_name}
+            )
 
-            flowbox.add(group_box)
+            client_switch.set_state(not self.server.client(client_name).muted)
+            name = Gtk.Label(self.server.client(client_name).friendly_name)
+
+            client_box.pack_start(name, False, False, 1)
+            client_box.pack_end(client_switch, False, False, 1)
+
+            client_container.pack_start(client_box, False, False, 0)
+
+        return client_container
+
+
+    def get_source_dropdown(self, group):
+        streams = []
+        # DROPDOWN
+        stream_store = Gtk.ListStore(str, str)
+        for stream in self.server.streams:
+            streams.append(stream.friendly_name)
+            stream_store.append([group.identifier, stream.friendly_name])
+
+        source_dropdown = Gtk.ComboBox.new_with_model_and_entry(stream_store)
+        source_dropdown.set_entry_text_column(1)
+
+        # If the stream isn't found, just use the first one
+        # This can because by changing the stream name on the server
+        try:
+            source_dropdown.set_active(streams.index(group.stream))
+        except:
+            source_dropdown.set_active(0)
+
+        source_dropdown.connect("changed", self.on_stream_changed)
+
+        source_dropdown_context = source_dropdown.get_style_context()
+        source_dropdown_context.add_class("dropdown")
+
+        source_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        source_label = Gtk.Label("Source")
+        source_label_context = source_label.get_style_context()
+        source_label_context.add_class("title")
+        source_label.set_halign(Gtk.Align.START)
+
+        source_container.pack_start(source_label, False, False, 1)
+        source_container.pack_end(source_dropdown, False, False, 1)
+        source_container.set_name("source-container")
+
+        return source_container
+
 
     def set_volume(self, volume):
         # set volume for client #0 to 50%
@@ -150,3 +136,42 @@ class SnapControlView(Gtk.FlowBox):
             self.loop.run_until_complete(
                 self.server.group_stream(group_id, stream_id)
             )
+
+
+    def update_group_list(self, flowbox):
+        for group in self.server.groups:
+            group_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+            group_box.set_name("group-box")
+
+            # HEADER
+            group_header_box = Gtk.Box()
+            group_header_box.set_name("group-header")
+
+            group_switch = Gtk.Switch()
+            group_switch.set_name("switch")
+            group_switch.connect("notify::active", self.on_mute_toggle, {"type": "group", "group": group})
+            group_switch.set_state(not group.muted)
+
+            group_name_label = Gtk.Label(group.friendly_name)
+
+            group_header_box.pack_start(group_name_label, False, False, 1)
+            group_header_box.pack_end(group_switch, False, False, 1)
+
+            group_box.pack_start(group_header_box, False, False, 1)
+
+            # SOURCE DROPDOWN
+            source_container = self.get_source_dropdown(group)
+
+            # SEPARATOR
+            group_box.pack_start(source_container, False, False, 1)
+            separator = Gtk.Separator()
+            group_box.pack_start(separator, False, False, 1)
+
+            separator_context = separator.get_style_context()
+            separator_context.add_class("separator")
+
+            # CLIENT LIST
+            clients = self.get_clients_box(group)
+            group_box.pack_start(clients, False, False, 1)
+
+            flowbox.add(group_box)
